26cc7dcda7d5d0a1badf173e9185d83d5eade192
Matthias Monnier <matthias.monnier@gmail.com>
2022-01-17 17:04:58 +0100

    Setup with tailwindcss + prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `personal-portfolio`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: `personal-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [`gatsby-plugin-postcss`],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
